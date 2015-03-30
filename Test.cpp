f0e3fed03ba1a4a2272955cad927a983d4349d93
YOUR NAME <YOUR EMAIL ADDRESS>
2015-03-30 18:54:28 +0600

    design Stack

---
+++ Test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack
+{
+    char a[10];
+    int top;
+
+
+public:
+
+    Stack()
+    {
+        top =0;
+    }
+    void push(char a);
+    char pop();
+};
+int main()
+{
+
+
+
+
+}
